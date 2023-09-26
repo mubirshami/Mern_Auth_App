39c1a4cdfc22678ac3079b7c060e8b0347e06911
mubirshami <mubirshami2000@gmail.com>
2023-09-26 23:20:01 +0500

    Home

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,28 @@
-const Home = () =>{
-    return(
-    <div className='home'>
-        <h2>Home</h2>
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/workoutDetails";
+
+const Home = () => {
+  const [workout, setworkout] = useState(null);
+  useEffect(() => {
+    const getWorkouts = async () => {
+      const response = await fetch("http://localhost:3000/api/workout");
+      const json = await response.json();
+
+      if(response.ok){
+        setworkout(json);
+      }
+    };
+    getWorkouts();
+  }, []);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workout && workout.map((workout)=>(
+            <WorkoutDetails key={workout._id} workout={workout}/>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
