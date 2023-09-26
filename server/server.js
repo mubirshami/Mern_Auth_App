c60da28f51539e767133c1df9dee88bfa29bc9c1
mubirshami <mubirshami2000@gmail.com>
2023-09-26 23:17:03 +0500

    Added CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3001",
+};
+
 
 const workoutRoutes = require("./routes/workout");
 
@@ -9,6 +14,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
   console.log(req.method, req.path);
